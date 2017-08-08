7ab5dab3107498d813ab4d6d500fc789f62f5064
Avineeshkumar <30717132+Avineeshkumar@users.noreply.github.com>
2017-08-08 14:39:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 var articleName=req.params.articleName;
-app.get('/articleName', function (req, res) {
+app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles(articleName)));
 });
 
