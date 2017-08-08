3ec7661ddb87d0cd70477691f13e35b078a16d19
Avineeshkumar <30717132+Avineeshkumar@users.noreply.github.com>
2017-08-08 20:30:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 var element=document.getElementById('main-text'
 );
 element.innerHTML="KTK";
+
+var img=document.getElementById('madi');
+img.onclick=function() {
+    img.style.marginLeft='100px';
+};
