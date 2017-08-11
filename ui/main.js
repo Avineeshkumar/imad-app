8f4526d67ded0781181442f1b85e195167371402
Avineeshkumar <30717132+Avineeshkumar@users.noreply.github.com>
2017-08-11 19:35:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,23 +1,16 @@
 var button=document.getElementById('counter');
-
+var counter=0;
 button.onclick=function(){
     
-  var request= new XMLHttpRequest();
   
-  
-  request.onreadystatechange=function(){
-      if(request.readyState === XMLHttpRequest.Done){ 
-          
-          if(request.status === 200){
-              var counter=request.responseText;
+              counter=counter+1;
               var span=document.getElementById('count');
               span.innerHTML=counter.toString();
-          }
-  }
+          
+          
   
-};
 
 
-request.open('GET','http://avineeshcse.imad.hasura-app.io/counter');
-request.send(null);
+
+
 };
