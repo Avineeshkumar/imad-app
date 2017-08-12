3bf8761e9072743f467e859722be20ff0978b39b
Avineeshkumar <30717132+Avineeshkumar@users.noreply.github.com>
2017-08-12 12:37:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -64,11 +64,10 @@ sbt.onclick=function(){
       if(request.readyState === XMLHttpRequest.DONE){ 
           
           if(request.status === 200){
-                var cmts=request.responseText;
-                cmts=JSON.parse(cmts);
+                cmt=JSON.parse(cmt);
                   var list='';
                   for(var i=0;i<cmts.length;i++){
-                      list+='<li>'+ cmts[i] +'</li>';
+                      list+='<li>'+ cmt[i] +'</li>';
                   }
                   
                   var ul=document.getElementById('cmtlist1');
