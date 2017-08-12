cedd70ff066280a32609460aa32df7deb044c800
Avineeshkumar <30717132+Avineeshkumar@users.noreply.github.com>
2017-08-12 13:37:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,30 @@
-var cmtInput=document.getElementById('comments1');
-var cmt=cmtInput.value;
-var sbt=document.getElementById('sbt1');
-sbt.onclick=function(){
+var button=document.getElementById('counter');
+
+button.onclick=function(){
+    
+  var request= new XMLHttpRequest();
+  
+  
+  request.onreadystatechange=function(){
+      if(request.readyState === XMLHttpRequest.DONE){ 
+          
+          if(request.status === 200){
+              var counter=request.responseText;
+              var span=document.getElementById('count');
+              span.innerHTML=counter.toString();
+          }
+  }
+  
+};
+
+
+request.open('GET','http://avineeshcse.imad.hasura-app.io/counter',true);
+request.send(null);
+};
+
+
+var submit=document.getElementById('sbt_btn');
+submit.onclick=function(){
       var request= new XMLHttpRequest();
   
   
@@ -9,22 +32,23 @@ sbt.onclick=function(){
       if(request.readyState === XMLHttpRequest.DONE){ 
           
           if(request.status === 200){
-              var cmts=request.responseText;
-                cmts=JSON.parse(cmts);
+                var names=request.responseText;
+                names=JSON.parse(names);
                   var list='';
-                  for(var i=0;i<cmts.length;i++){
-                      list+='<li>'+ cmts[i] +'</li>';
+                  for(var i=0;i<names.length;i++){
+                      list+='<li>'+ names[i] +'</li>';
                   }
                   
-                  var ul=document.getElementById('cmtlist1');
+                  var ul=document.getElementById('namelist');
                   ul.innerHTML=list;
                           }
                   }
   
 };
 
-
-request.open('GET','http://avineeshcse.imad.hasura-app.io',true);
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+request.open('GET','http://avineeshcse.imad.hasura-app.io/submit-name?name='+name,true);
 request.send(null);
 
 };
