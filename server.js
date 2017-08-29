c54d0cb8901afd1f3ec49a2d6f1907b35ebba74a
Avineeshkumar <30717132+Avineeshkumar@users.noreply.github.com>
2017-08-29 13:48:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=require('pg').pool;
 
+var config={
+user:'avineeshcse',
+database:'avineeshcse',
+host:'http://db.imad.hasura-app.io',
+port:'5432',
+password:process.emv.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -86,6 +94,17 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var pool=new Pool(config);
+app.get('/test-db', function (req, res) {
+  pool.query('SELCT * FROM test',function (err, result){
+   if(err)  {
+       res.status(500).send(err.toString());
+   } else
+   {
+       res.send(JSON.stringify(result));
+   }
+  });
+});
 
 var counter=0;
 app.get('/counter', function (req, res) {
@@ -108,7 +127,7 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/ui/style.css', function (req, res) {
+app.get('/ui/style.css', fun ction (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
